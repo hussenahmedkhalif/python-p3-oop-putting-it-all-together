e05a8a60fb8f2fa6ce0f6eddc72828fd60a7f28f
husseinahmed <hussenahmedkhalif122@gmail.com>
2024-08-15 23:17:54 +0300

    complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Initialize a private attribute
+        self.page_count = page_count  # Use the setter to validate the initial value
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Initialize a private attribute
+        self.size = size  # Use the setter to validate the initial value
+        self.condition = "Used"  # Initialize condition attribute
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"  # Set condition to "New" after repair
